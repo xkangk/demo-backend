e991e5c306a815789fb0d6092b145e4b4905d099
xkangk <n0jaka00@students.oamk.fi>
2021-10-18 20:29:36 +0300

    Nyt se ryökäle toimii

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
-header("Access-Control-Allow-Origin; " . $_SERVER["HTTP_ORIGIN"]);
+header("Access-Control-Allow-Origin:" . $_SERVER["HTTP_ORIGIN"]);
 header("Access-Control-Allow-Credentials:true");
-header("Access-Control-Allow-Method: GET,POST,PUT;DELETE;OPTIONS");
+header("Access-Control-Allow-Method: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Accept, Content-type", "Access-Control-Allow-Header");
 header("Access-Control-Allow-Max-Age: 3600");
-header("Content-Type:application/json");
+header("Content-Type: application/json");
 
-if($_SERVER["REQUEST"]==="OPTIONS") {
+if ($_SERVER["REQUEST_METHOD"]==="OPTIONS") {
     return 0;
 }
 
 $input  = json_decode(file_get_contents("php://input"));
 $description = filter_var($input->description,FILTER_SANITIZE_STRING);
 
-try{
-    $db = new PDO("mysql:host=localhost;dbname=todo;charset=utf8","root", "");
+try {
+    $db = new PDO("mysql:host=localhost;dbname=todo;charset=utf8","root","");
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
     $query = $db->prepare("insert into task(description) values (:description)");
     $query->bindValue(":description", $description,PDO::PARAM_STR);
-    $query->excecute();
+    $query->execute();
+
     header("HTTP/1.1. 200 OK");
     $data = array("id" => $db->lastInsertId(),"description" => $description);
     print json_encode($data);
